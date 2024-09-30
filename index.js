ac9f1737af111b4551aecb79b8a6f8692c86cf12
IulikTKW <inicula1989uk@gmail.com>
2024-09-30 20:25:13 +0300

    1

---
+++ index.js
@@ -0,0 +1,148 @@
+// var let const
+
+// var x = 1;
+
+// let y = 2;
+
+// const z = 3;
+
+// z = 'new';
+
+// console.log(z);
+
+// const num = 1;
+// const str = 'string';
+// const bool = true;
+// const undief = undefined;
+// const nul = null;
+// const symb = Symbol;
+// const bigInt = BigInt(11);
+
+// const obj1 = {
+//     a: 4,
+//     b: 5,
+//     c: 6
+// }
+
+// adresa de la 2-4
+
+// const obj2 = obj1
+
+//adresa de la 2-4
+
+// obj1.a= 99;
+
+
+// console.log('obj1', obj1)
+// console.log('obj2', obj2)
+
+// let num1 = 1;
+
+// let num2 = num1;
+
+// num1 = 4;
+
+// console.log('num1', num1)
+// console.log('num2', num2)
+
+// const num1 = 3;
+
+// const num2 = 4;
+
+// console.log(num1 + num2)
+
+// const str1 = 'string1';
+// const str2 = '3';
+
+// console.log(str1 + '3' + '4');
+
+// console.log(num2 - str1)
+
+// console.log(NaN == NaN)
+
+
+// const num1 = 3;
+// console.log('type of num1 is:', typeof num1)
+
+// const str = 'string';
+// console.log('type of str is:', typeof str)
+
+// const bool = true;
+// console.log('type of bool is:', typeof bool)
+
+// const undief = undefined;
+// console.log('type of undief is:', typeof undief)
+
+// const nul = null
+// console.log('type of nul is:', typeof nul)
+
+// const bigInt = BigInt(33)
+// console.log('type of bigInt is:', typeof bigInt)
+
+// const symb = Symbol('2')
+// console.log('type of symb is:', typeof symb)
+
+// const obj = {};
+// console.log('type of num1 is:', typeof obj)
+
+
+// console.log('adunare', 3 + 3)
+// console.log('scaderea', 2 - 3)
+// console.log('concatinare', 'string' + ' string2')
+
+// console.log(0 == false)
+// console.log(0 === false)
+
+// console.log(3 == 5)
+// console.log(3 === '3')
+
+// const falsy1 = 0;
+// const falsy2 = ''
+// const falsy3 = undefined;
+// const falsy4 = null;
+// const falsy5 = NaN;
+
+// comparatie intre falsy value (odata true sa fie rezultat odata false)   incriment si decrement 
+
+console.log(0 == '')
+console.log(0 === '')
+
+let num1 = 3
+
+console.log(++num1)
+console.log(--num1)
+
+// console.log(!!falsy1)
+// console.log(Boolean(falsy2))
+// console.log(Boolean(falsy3))
+// console.log(Boolean(falsy4))
+// console.log(Boolean(falsy5))
+
+
+// console.log(3*5)
+// console.log(3/5)
+
+// let num1 = 5
+
+// num1 = num1 + 5
+
+// num1 = 5 +6
+
+// num1+= 6
+
+
+// console.log(++num1)
+
+// console.log(num1)
+
+// console.log(num1)
+
+// let decr = 6
+
+// decr-=2
+
+// console.log(--decr)
+
+// console.log(3**3)
+// console.log(2**2)
+
